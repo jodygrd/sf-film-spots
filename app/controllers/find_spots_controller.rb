a62db17434219756ddd63c40c169cb690b2b6217
Jody Lipkin <lipkinjody@gmail.com>
2017-04-02 11:12:19 -0700

    weather app

--- app/controllers/find_spots_controller.rb
+++ app/controllers/find_spots_controller.rb
@@ -1,7 +1,26 @@
 class FindSpotsController < ApplicationController
 	def index
 		@spots = Unirest.get("https://data.sfgov.org/resource/wwmu-gmzc.json").body
+	end
 
+	def weather
+		@city = params[:city] 
+		@state = params[:state]
+
+		if @state && @city
+			@weather_data = Unirest.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22#{@city}%2C%20#{@state}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys").body
+		else
+			@weather_data = Unirest.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22sanfrancisco%2C%20ca%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys").body
+		end
+	end
+
+	def search
+	end
+
+	def update
+		@city = params[:city] 
+		@state = params[:state]
+		redirect_to "/weather?city=#{@city}&state=#{@state}"
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
 	get "/spots" => "find_spots#index"
+	get "/weather" => "find_spots#weather"
+	
+	get "/search" => "find_spots#search"
+	post "/weather" => "find_spots#weather"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
